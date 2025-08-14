78c6ea8fe2375ebfda719de64b39d0ca8a2c1f36
settra7 <settra7@gmail.com>
2025-08-14 12:32:50 +0200

    Added Day 4 Project File

---
+++ week1_project/day4_project.py
@@ -0,0 +1,11 @@
+# Practice:
+# Write a function that takes a list of numbers and returns their average.
+# Call this function and print the result.
+
+
+def list_average(num_array):
+    print(sum(num_array) / len(num_array))
+
+x = [12, 14, 16, 18, 20]
+
+list_average(x)
